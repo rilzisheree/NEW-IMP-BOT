0ca6d98ef005a7daef7fd6635e58f125e6b85e1c
rilzisheree <160282181+rilzisheree@users.noreply.github.com>
2026-06-05 17:45:49 +0300

    Delete src/models/ValorChannel.js

--- src/models/ValorChannel.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model, models } from 'mongoose';
-
-const valorChannelSchema = new Schema({
-  guildId:   { type: String, required: true, unique: true },
-  channelId: { type: String, required: true },
-}, { timestamps: true });
-
-export default models.ValorChannel || model('ValorChannel', valorChannelSchema);
