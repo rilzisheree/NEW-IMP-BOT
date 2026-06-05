e612da85f79491de8dcbd40283902fc882dfd765
rilzisheree <160282181+rilzisheree@users.noreply.github.com>
2026-06-05 17:22:18 +0300

    Delete src/models/LoreteamRole.js

--- src/models/LoreteamRole.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model, models } from 'mongoose';
-
-const loreteamRoleSchema = new Schema({
-  guildId: { type: String, required: true, unique: true },
-  roleId:  { type: String, required: true },
-}, { timestamps: true });
-
-export default models.LoreteamRole || model('LoreteamRole', loreteamRoleSchema);
